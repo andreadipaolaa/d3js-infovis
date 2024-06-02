5268f2d81a4e791972af0e83de15d5f0e6e806ff
Andrea Di Paola <andrea.dipaola@outlook.com>
2024-06-02 19:21:01 +0200

    aggiunta prima animazione

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ heightScale.domain(yDomain);
 
 let currentXHome = 10;
 let xRange = 0;
-let yRange=0;
+let yRange = 0;
 let currentHeight = 200;
-
 
 function buildHome(data) {
     const homeWidth = parseFloat(xScale(data.homeWidth));
@@ -21,24 +20,18 @@ function buildHome(data) {
     const roofHeight = parseFloat(heightScale(data.roofHeight));
     const y = parseFloat(yScale(currentHeight));
 
-    // Calcola le coordinate della casa
     let home = `${currentXHome},${y} ` +
                `${currentXHome + homeWidth / 2},${y} ` +
                `${currentXHome + homeWidth / 2},${y - homeHeight} ` +
                `${currentXHome - homeWidth / 2},${y - homeHeight} ` +
                `${currentXHome - homeWidth / 2},${y}`;
 
-    // Calcola le coordinate del tetto
     let roof = `${currentXHome},${y - homeHeight} ` +
                `${currentXHome + roofWidth / 2},${y - homeHeight} ` +
                `${currentXHome},${y - homeHeight - roofHeight} ` +
                `${currentXHome - roofWidth / 2},${y - homeHeight}`;
 
-    // Trova la larghezza maggiore tra homeWidth e roofWidth e aggiorna currentXHome
-    currentXHome +=  xRange+ 10; 
-
-    console.log(`Home coordinates: ${home}`);
-    console.log(`Roof coordinates: ${roof}`);
+    currentXHome += xRange + 10;
 
     return { home, roof };
 }
@@ -47,25 +40,64 @@ function fillBoard(svgBoard, data) {
     let homes = svgBoard.selectAll(".home")
         .data(data)
         .enter()
-        .append("g"); // Raggruppa ogni casa e tetto in un elemento <g>
-    var n=0
+        .append("g")
+        .attr("class", "house");
+
+    let n = 0;
     homes.each(function(d) {
-        if(n==5){
-            currentXHome = 10 + (xRange/2);
-            currentHeight -= (100+(2*yRange));
+        if (n === 5) {
+            currentXHome = 10 + (xRange / 2);
+            currentHeight -= (70 + (2 * yRange));
         }
         const houseData = buildHome(d);
         n += 1;
+
         d3.select(this).append("polygon")
             .attr("fill", "yellow")
             .attr("class", "home")
-            .attr("points", houseData.home);
+            .attr("points", houseData.home)
+            .attr("data-case", d.homeWidth) // Aggiungi l'attributo data-case
+            .on("click", function() { sortHouses("homeWidth"); }); // Event listener
 
         d3.select(this).append("polygon")
             .attr("fill", "brown")
             .attr("class", "roof")
-            .attr("points", houseData.roof);
+            .attr("points", houseData.roof)
+            .attr("data-case", d.roofWidth) // Aggiungi l'attributo data-case
+            .on("click", function() { sortHouses("roofWidth"); }); // Event listener
     });
+}
+
+function sortHouses(property) {
+    d3.json("data.json")
+        .then(function(data) {
+            data.sort((a, b) => d3.ascending(a[property], b[property]));
+
+            let homes = d3.select("#svg-board").selectAll(".house");
+
+            // Rimuovere gli elementi esistenti con una transizione di scomparsa
+            homes.transition()
+                .duration(1000)
+                .style("opacity", 0)
+                .remove()
+                .on("end", function() {
+                    // Una volta rimossi, ricostruire la board con i dati ordinati
+                    currentXHome = 10;
+                    currentHeight = 200;
+                    let svgBoard = d3.select("#svg-board");
+                    currentXHome += (xRange / 2);
+                    currentHeight += (70 + (2 * yRange));
+                    fillBoard(svgBoard, data);
+
+                    // Appare con una transizione
+                    d3.select("#svg-board").selectAll(".house")
+                        .style("opacity", 0)
+                        .transition()
+                        .duration(1000)
+                        .style("opacity", 1);
+                });
+        })
+        .catch(error => console.log(error));
 }
 
 d3.json("data.json")
@@ -76,15 +108,13 @@ d3.json("data.json")
         let svgBoard = d3.select("#svg-board");
         svgBoard.attr("width", boardWidth);
         svgBoard.attr("height", boardHeight);
-        xRange = boardWidth/6;
-        yRange = boardHeight/5;
-        currentXHome += (xRange/2);
-        currentHeight += (100+(2*yRange));
+        xRange = boardWidth / 6;
+        yRange = boardHeight / 5;
+        currentXHome += (xRange / 2);
+        currentHeight += (70 + (2 * yRange));
         xScale.range([0, xRange]);
         yScale.range([boardHeight, 0]);
         heightScale.range([0, yRange]);
-
-        console.log(`Board dimensions: ${boardWidth}x${boardHeight}`);
 
         fillBoard(svgBoard, data);
     })
