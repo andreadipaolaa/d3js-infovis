58f1b13b0508d76a80e0f0100de527cd466d9ee0
Andrea Di Paola <andrea.dipaola@outlook.com>
2024-06-02 19:07:18 +0200

    Sistemati problemi grafici

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ let yScale = d3.scaleLinear();
 let heightScale = d3.scaleLinear();
 
 const xDomain = [0, 1000];
-const yDomain = [0, 800];
+const yDomain = [0, 1000];
 xScale.domain(xDomain);
 yScale.domain(yDomain);
 heightScale.domain(yDomain);
 
-let currentXHome = 100;
+let currentXHome = 10;
+let xRange = 0;
+let yRange=0;
+let currentHeight = 200;
+
 
 function buildHome(data) {
     const homeWidth = parseFloat(xScale(data.homeWidth));
     const homeHeight = parseFloat(heightScale(data.homeHeight));
     const roofWidth = parseFloat(xScale(data.roofWidth));
     const roofHeight = parseFloat(heightScale(data.roofHeight));
-    const y = parseFloat(yScale(100));
+    const y = parseFloat(yScale(currentHeight));
 
     // Calcola le coordinate della casa
     let home = `${currentXHome},${y} ` +
@@ -31,8 +35,7 @@ function buildHome(data) {
                `${currentXHome - roofWidth / 2},${y - homeHeight}`;
 
     // Trova la larghezza maggiore tra homeWidth e roofWidth e aggiorna currentXHome
-    const maxWidth = Math.max(homeWidth, roofWidth);
-    currentXHome += maxWidth + 40; 
+    currentXHome +=  xRange+ 10; 
 
     console.log(`Home coordinates: ${home}`);
     console.log(`Roof coordinates: ${roof}`);
@@ -45,10 +48,14 @@ function fillBoard(svgBoard, data) {
         .data(data)
         .enter()
         .append("g"); // Raggruppa ogni casa e tetto in un elemento <g>
-
+    var n=0
     homes.each(function(d) {
+        if(n==5){
+            currentXHome = 10 + (xRange/2);
+            currentHeight -= (100+(2*yRange));
+        }
         const houseData = buildHome(d);
-
+        n += 1;
         d3.select(this).append("polygon")
             .attr("fill", "yellow")
             .attr("class", "home")
@@ -63,16 +70,19 @@ function fillBoard(svgBoard, data) {
 
 d3.json("data.json")
     .then(function(data) {
-        const boardHeight = Math.floor(0.8 * window.screen.height);
-        const boardWidth = Math.floor(0.8 * window.screen.width);
+        const boardHeight = Math.floor(0.9 * window.screen.height);
+        const boardWidth = Math.floor(0.9 * window.screen.width);
 
         let svgBoard = d3.select("#svg-board");
         svgBoard.attr("width", boardWidth);
         svgBoard.attr("height", boardHeight);
-
-        xScale.range([0, boardWidth/3]);
+        xRange = boardWidth/6;
+        yRange = boardHeight/5;
+        currentXHome += (xRange/2);
+        currentHeight += (100+(2*yRange));
+        xScale.range([0, xRange]);
         yScale.range([boardHeight, 0]);
-        heightScale.range([0, boardHeight]);
+        heightScale.range([0, yRange]);
 
         console.log(`Board dimensions: ${boardWidth}x${boardHeight}`);
 
